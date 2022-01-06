aec99094b829fa2f5a5bd9942ff721b509fa9567
jadely-lab <jadely0819@gmail.com>
2022-01-06 14:51:21 +0900

    Add new book

---
+++ BOOK_200prob/Part_1/ex009.js
@@ -0,0 +1,15 @@
+var str = 'JavaScript';
+var num = 200;
+var arr = [1,2,3,4,5];
+var obj = {a:1, b:2, c:3};
+
+console.log(str);
+console.log(num);
+console.log(arr);
+console.log(obj);
+
+// 다른 console 메소드
+console.debug('log와 동일한 로그 메세지를 출력합니다.');
+console.error('에러 메세지를 출력합니다.')
+console.info('정보성 메세지를 출력합니다.')
+console.warn('경고성 메세지를 출력합니다.')
